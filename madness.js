7e1d4e6a53248f61c9c787cdd9d11611f8930443
Kevin Hu <kevinhuwest@gmail.com>
2016-03-29 20:49:25 -0400

    write to file

--- madness.js
+++ madness.js
@@ -1,6 +1,7 @@
 var util = require('util');
 var startDate = new Date();
 var startTime = startDate.getTime();
+var fs = require('fs');
 var combine = function(a) {
   var fn = function(n, src, got, all) {
     if (n == 0) {
@@ -25,7 +26,6 @@ var combine = function(a) {
 function dec2bin(dec) {
   return (dec >>> 0).toString(2);
 }
-
 var round1 = [
   [1, 16],
   [8, 9],
@@ -82,7 +82,7 @@ var round2Pairs = round2.map(function(teamList) {
 var round3 = [];
 
 for (m = 0; m < round2Pairs.length; m++) {
-  console.log(m);
+  //console.log(m);
   var round2Result = [];
   round2Result = roundResults(round2Pairs[m]);
   round3.push(round2Result);
@@ -95,10 +95,31 @@ var round3Pairs = round3.map(function(teamList) {
   return subPair;
 });
 
-
-console.log(round3[0]);
 console.log(util.inspect(round3Pairs, false, null));
+console.log(round3Pairs.length)
 
+var round4 = [];
+
+for (o = 0; o<round3Pairs.length;o++){
+  for (p=0;p<round3Pairs[o].length;p++){
+
+  }
+}
+
+//WRITE TO JSON FILE
+require('fs').writeFile(
+
+    'combinations.json',
+
+    JSON.stringify(round3Pairs, null, 2),
+
+    function (err) {
+        if (err) {
+            console.error('¯\_(ツ)_/¯ Stuff happens...');
+        }
+    }
+);
+//TIME ELAPSED
 endDate = new Date();
 var endTime = endDate.getTime();
 console.log("\n  Total time elapsed:" + (endTime - startTime) / 1000 + "s\n");

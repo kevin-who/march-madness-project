037db12421ad7692eb80038c3eeb221725aef31d
Kevin Hu <kevinhuwest@gmail.com>
2016-04-01 15:12:48 -0400

    removed repos

--- inspire-tree-master/demos/require.config.js
+++
@@ -1,7 +0,0 @@
-require.config({
-    paths: {
-        jquery: 'https://code.jquery.com/jquery-2.2.2.min',
-        inspireTree: '../dist/inspire-tree.min',
-        lodash: 'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.6.1/lodash'
-    }
-});

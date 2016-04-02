c4a4ecba3b8c19719665dc02559b7b856c887795
Kevin Hu <kevinhuwest@gmail.com>
2016-04-02 13:14:04 -0400

    pairs

--- jsonformat.js
+++ jsonformat.js
@@ -2,7 +2,14 @@ var util = require('util');
 var startDate = new Date();
 var startTime = startDate.getTime();
 var fs = require('fs');
-
+function pairs(arr) {
+  var groups = [],
+    i;
+  for (i = 0; i < arr.length; i += 2) {
+    groups.push(arr.slice(i, i + 2));
+  }
+  return groups;
+}
 var unformatTree = JSON.parse(fs.readFileSync('combinations32768.json', 'utf8'));
 var name = "1,16,8,9,5,12,4,13,6,11,3,14,7,10,2,15"
 var children = []
@@ -12,24 +19,24 @@ for (var round1 in unformatTree) {
   children.push(hash);
   delete unformatTree[round1]
 }
-unformatTree["name"] = name;
+unformatTree["name"] = ("[" + pairs(name.split(",")).join("] [") + "]").replace(/,/g," vs ")
 unformatTree["children"] = children;
 
 for (i = 0; i < unformatTree["children"].length; i++) {
-  unformatTree["children"][i]["name"] = Object.keys(unformatTree["children"][i])[0]
+  unformatTree["children"][i]["name"] = ("[" + pairs(Object.keys(unformatTree["children"][i])[0].split(",")).join("] [") + "]").replace(/,/g," vs ")
   children1 = []
   childrenHash = unformatTree["children"][i][Object.keys(unformatTree["children"][i])[0]]
   for(round3 in childrenHash){
     var hash = {}
-    hash["name"] = round3
+    hash["name"] = ("[" + pairs(round3.split(",")).join("] [") + "]").replace(/,/g," vs ")
     hash["children"] = []
     for( round4 in childrenHash[round3]){
       var hash1 = {}
-      hash1["name"] = round4
+      hash1["name"] = ("[" + pairs(round4.split(",")).join("] [") + "]").replace(/,/g," vs ")
       hash1["children"]=[]
       for (round5 in childrenHash[round3][round4]){
         nameHash = {}
-        nameHash["name"] = round5
+        nameHash["name"] = ("[" + pairs(round5.split(",")).join("] [") + "]").replace(/,/g," vs ")
         hash1["children"].push(nameHash)
       }
       hash["children"].push(hash1)
@@ -39,11 +46,11 @@ for (i = 0; i < unformatTree["children"].length; i++) {
   unformatTree["children"][i]["children"] = children1
   delete unformatTree["children"][i][Object.keys(unformatTree["children"][i])[0]];
 }
-formatTree = unformatTree
+formatTree = [unformatTree]
 //WRITE TO JSON FILE
 require('fs').writeFile(
 
-  'circle32768.json',
+  'tree32768.json',
 
   JSON.stringify(formatTree, null, 2),
 
